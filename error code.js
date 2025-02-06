051747bcb68d8d4ce5d3c40ce27f5dfea049809a
Alex Kurcan <akurcan@my.waketech.edu>
2025-02-06 13:54:33 -0500

    adding files and code python

---
+++ error code.js
@@ -0,0 +1,8 @@
+// Challenge 1 - Control Structures (if-else statements)
+// Write a program in JaveScipt that checks if a number is even or odd.
+
+// Write a program in JaveScipt that checks if a person is eligible to vote based on their age.
+
+// Write a program in JaveScipt that checks if a number is positive, negative, or zero, and displays the appropriate message.
+
+// Write a program in JaveScipt that prints numbers from 1 to 10 using a for loop.
