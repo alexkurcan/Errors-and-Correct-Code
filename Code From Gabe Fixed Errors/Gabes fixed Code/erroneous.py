4f4ac03b1f4d790f313945d62334551fea97a9ed
Alex Kurcan <akurcan@my.waketech.edu>
2025-02-07 14:00:56 -0500

    adding

---
+++ Code From Gabe Fixed Errors/Gabes fixed Code/erroneous.py
@@ -0,0 +1,118 @@
+# There THREE errors in this code
+# # Challenge 1 - Control Structures (if-else statements)
+#
+# # Write a program in Python that checks if a number is even or odd.
+def odd_or_even(userinput):
+    if userinput % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+
+userinput_number = int(input("Enter any number:"))
+print(odd_or_even(userinput_number))
+
+# # Write a program in Python that checks if a person is eligible to vote based on their age.
+
+def checking_voting_age(x): ## GABE CHANGE # 3 - Changed == operater to >= operator.
+    if x >= 18:
+        return "You are old enough to vote."
+    else:
+        return "you are not eligible to vote."
+
+user_input_age = int(input("Enter your age please: "))
+print(checking_voting_age(user_input_age))
+
+# # Write a program in Python that checks if a number is positive, negative, or zero, and displays the appropriate message.
+def pos_neg_zero(x):
+    if x >= 0:
+        return f"{x} is positive"
+    if x == 0:
+        return "The number is neither odd or even, it is zero"
+    if x < 0:
+        return f"{x} is negative" 
+   
+user_input = int(input("Enter a number: "))
+print(pos_neg_zero(user_input))
+
+#----------------------------------------------------------------------------
+# # Challenge 2 - Loops (for and while loops)
+#
+# # Write a program in Python that prints numbers from 1 to 10 using a for loop.
+i =" "
+for i in range(1,11):
+    print(i)
+
+# Write a program in Python that prints numbers from 1 to 5 using a while loop.
+i = 0
+while i < 5:
+    i += 1
+    print(i)
+
+# # Write a program in Python that calculates the factorial of a given number using a loop.
+
+factorial = 1
+num = int(input("Enter a number: "))
+for i in range(1, num+1):
+    factorial *= i ## GABE CHANGE # 4 - Changed the operator from + to *. 
+
+print(factorial)
+
+#----------------------------------------------------------------------------
+# Challenge 3 - Functions and Scopes
+#
+# # Write a function in Python that greets a user by name.
+def greeting_by_username(username):
+    return f'Hello {username}, welcome to blah blah'
+
+username = input("Enter your username: ")
+print(greeting_by_username(username))
+
+# # Write a function in Python that checks if a number is even or odd.
+def even_or_odd(x):
+    if x % 2 == 0:
+        return 'even'
+    else:
+        return "odd"
+
+user_input = int(input("Enter a number: "))
+print(even_or_odd(user_input))
+
+# # Write a function in Python that accepts a string and returns the number of vowels in the string
+def checking_for_vowels(x):
+    count = 0
+    vowels = "AEIOUaeiou"
+    for i in x:
+        if i in vowels:
+            count += 1
+        print(count)
+
+word = input("Enter a word: ") ## GABE CHANGE # 4.5 - Changed "eneter" to "enter". (I know this was just a typo, just wanted to fix it.)
+print(checking_for_vowels(word))
+
+#----------------------------------------------------------------------------
+# Challange 4 - Arrays and Objects
+#
+# # Write a program in Python that stores a list of fruits and prints each fruit using a loop.
+fruits = ['grapes', 'apples', 'grapfruit', 'oranges', 'banana', "kiwi", "dragonfruit"]
+for i in fruits:
+    print(i)
+
+# #Write a program in Python that stores student names and their grades in a dictionary, and prints each student's name and grade.
+students_in_class = {
+    "Shimmer Sham" : 100,
+    "Khani Lyan" : 100,
+    "Jane Smith" : 92,
+    "John Smith" : 1
+}
+
+for key, value in students_in_class.items():
+    print(f"{key}: {value}")
+
+# # Create an array of numbers in Python and write a function to find the highest number in the array
+numbers = [1, 5, 1000, 99, 100, 80, 82]
+def finding_largest_number(num):
+    num.sort()
+    largest = num[-1]
+    return largest
+
+finding_largest_number(numbers)
--- Correct Code/javascript_code.js
+++ Correct Code/javascript_code.js
@@ -1,21 +1,22 @@
 // Challenge 1 - Control Structures (if-else statements)
 
-// Write a program in JavaScript that checks if a number is even or odd.
+// 1a: Odd or Even
+
 function isEvenOrOdd(x) {
-    return(x % 2 ==0);
+  return(x % 2 ==0);
 }
 
 let x = 22;
 isEvenOrOdd(x) ? console.log("Even") : console.log("Odd");
 
-// Write a program in JavaScript that checks if a person is eligible to vote based on their age.
+// 1b: Eligible to vote
 
 function checkVotingEligibility(age) {
-    if (age >= 18) {
-        console.log("You are eligible to vote.");
-    } else {
-        console.log("You are not eligible to vote.");
-    }
+  if (age >= 18) {
+      console.log("You are eligible to vote.");
+  } else {
+      console.log("You are not eligible to vote.");
+  }
 }
 
 let age = prompt("Please enter your age: ");
@@ -23,28 +24,60 @@ age = parseInt(age);
 
 checkVotingEligibility(age);
 
-// Write a program in JavaScript that checks if a number is positive, negative, or zero, and displays the appropriate message.
+// 1c: Check pos neg zerofunction checkNumberNegativeOddEven(num) {
 function checkNumberNegativeOddEven(num) {
-    if (num > 0) {
-      console.log("The number is positive.");
-    } else if (num < 0) {
-      console.log("The number is negative.");
-    } else {
-      console.log("The number is zero.");
-    }
+  if (num > 0) {
+    console.log("The number is positive.");
+  } else if (num < 0) {
+    console.log("The number is negative.");
+  } else {
+    console.log("The number is zero.");
   }
-  
-  let userinput = prompt("Enter a number: ");
-  userinput = parseFloat(userinput);
-  checkNumberNegativeOddEven(userinput);
+}
 
-// Challenge 2 - Loops (For and While Loops)
 
-// Write a program in JaveScipt that prints numbers from 1 to 10 using a for loop.
+let userinput = prompt("Enter a number: ");
+userinput = parseFloat(userinput);
+checkNumberNegativeOddEven(userinput);
+
+
+// 2a: Print 1-10 for loop
+
 for (let x = 1; x <= 10; x++) {
-    console.log(x);
-  }
+  console.log(x);
+}
 
+// 2b: Print 1-5 while loop
+i = 1
+while (i < 6 && i>0){
+  console.log(i)
+  i += 1
+}
+
+// 2c: Factorial
+let facnum = 5
+let fact = 1
+for(let i=1;i<=facnum;i++){
+  fact = fact *i
+}
+console.log(fact)
+
+// 3a: Greeting user
+let username = prompt("Give your name: ")
+function Greetings(name){
+  console.log('Hello, ${name}')
+}
+
+// 3b: Even or odd, function
+function EvenOdd(){
+  let num = 26
+  if (num%2 === 0) {
+      console.log("Even")
+  }
+  else{
+       console.log("Odd")
+  }
+}
 // 3c: Number of vowels
 
 function getCount(str) {
@@ -75,7 +108,7 @@ let studentGrades = {
 };
 
 for (let student in studentGrades) {
-  console.log(student + ": " + student) //  console.log(student + ": " + studentGrades[student])
+  console.log(student + ": " + studentGrades[student])
 }
 // 4c: Highest number in array
 
@@ -91,4 +124,3 @@ function highestNum(n) {
 
 let numarray = [1, 5, 17, 200, 2, 9, 37, 91];
 console.log(highestNum(numarray));
-
